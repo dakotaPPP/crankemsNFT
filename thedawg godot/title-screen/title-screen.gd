23eef3adacadbc6c99c9c5e0eb6d64fef1d4a8dd
dakotaPPP <76417971+dakotaPPP@users.noreply.github.com>
2022-02-16 12:11:31 -0600

    yo

--- thedawg godot/title-screen/title-screen.gd
+++ thedawg godot/title-screen/title-screen.gd
@@ -9,6 +9,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$menuBtns/playBtn.grab_focus()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,3 +28,15 @@ func _on_optionBtns_pressed():
 
 func _on_exitBtn_pressed():
 	get_tree().quit() # Replace with function body.
+
+
+func _on_background_ready():
+	var viewportWidth = get_viewport().size.x
+	var viewportHeight = get_viewport().size.y
+	var scale = viewportWidth / $Sprite.texture.get_size().x 
+
+
+func _on_logo_ready():
+	var logos = ["cranklogo","goldlogo","chowderLogo"]
+	var item = logos[randi() % logos.size()]
+	$Image.texture = load("res://title-screen/"+item+".png")

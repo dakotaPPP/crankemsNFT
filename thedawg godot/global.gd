24348f720ea2673c6339effccf4c5866fdb102e3
dakotaPPP <76417971+dakotaPPP@users.noreply.github.com>
2022-04-19 14:15:06 -0500

    MOPVING BUTTON

--- thedawg godot/global.gd
+++ thedawg godot/global.gd
@@ -2,15 +2,11 @@ extends Node
 class_name GlobalClass
 
 
-static func buttonPress(btn,kinebody):
-	var speed = 200
-	var velocity = kinebody.getVelocity()
-	velocity.y += 10
-	velocity = velocity * speed
-	velocity = kinebody.move_and_slide(velocity)
-	kinebody.startMovin()
-	#add while statement here
-	kinebody.stopMovin()
+static func buttonDown(aniPlr):
+	aniPlr.set_current_animation("buttonDown")
+	
+static func buttonUp(aniPlr):
+	aniPlr.queue("buttonUp")
 
 
 
--- thedawg godot/play-screen/playScene.gd
+++ thedawg godot/play-screen/playScene.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-
 func _on_TextureButton_button_down():
-	GlobalClass.buttonPress(get_node("KinematicBody2D/TextureButton"),get_node("KinematicBody2D"))
-	
+	get_node("button1/AnimationPlayer").set_current_animation("buttonDown")
+
+
+func _on_TextureButton_button_up():
+	get_node("button1/AnimationPlayer").set_current_animation("buttonUp")
+	get_tree().change_scene("res://pack-screen/packScene.tscn") 
